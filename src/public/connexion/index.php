e153611836815d3096a1dd3b833a7f926d58f44f
Linguiny0 <rafimunch@orange.fr>
2026-04-15 18:12:37 +0200

    Code connexion\index.php pour la page web + login.php pour la méthode de connexion via la db

--- src/public/connexion/index.php
+++ src/public/connexion/index.php
@@ -3,29 +3,20 @@ include '../../templates/header.php';
 ?>
 
     <main>
-
         <h2>Connexion</h2>
-
         <form method="POST" action="login.php">
-
             <div>
                 <label for="email">Email :</label><br>
                 <input type="email" id="email" name="email" required>
             </div>
-
             <br>
-
             <div>
                 <label for="password">Mot de passe :</label><br>
                 <input type="password" id="password" name="password" required>
             </div>
-
             <br>
-
             <button type="submit">Se connecter</button>
-
         </form>
-
     </main>
 
 <?php
