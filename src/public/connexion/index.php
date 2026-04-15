2ab6514ee4ff01cd8815eddd87d75a03f7483a6e
Linguiny0 <rafimunch@orange.fr>
2026-04-15 18:01:14 +0200

    Création de la page de connexion

--- src/public/connexion/index.php
+++ src/public/connexion/index.php
@@ -1,15 +1,33 @@
 <?php
-
 include '../../templates/header.php';
-
 ?>
 
+    <main>
 
-<main>
+        <h2>Connexion</h2>
 
-    <h2>
-        Connexion
-    </h2>
+        <form method="POST" action="login.php">
 
+            <div>
+                <label for="email">Email :</label><br>
+                <input type="email" id="email" name="email" required>
+            </div>
 
-</main>
+            <br>
+
+            <div>
+                <label for="password">Mot de passe :</label><br>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+            <br>
+
+            <button type="submit">Se connecter</button>
+
+        </form>
+
+    </main>
+
+<?php
+include '../../templates/footer.php';
+?>
