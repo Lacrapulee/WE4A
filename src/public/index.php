d46363b6ff319bbd07d9f24c2c4b1469f8006f80
Manon <pubs.mg@gmail.com>
2026-04-30 15:41:41 +0200

    redirection vers catalogue

--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,4 @@
-<?php 
-    require_once '../includes/db.php';
-    require_once '../includes/articles_functions.php'; // On charge nos fonctions
-    
-    // On appelle la fonction pour récupérer les données
-    $articles = getAnnoncesEnLigne($pdo);
-
-    include '../templates/header.php'; 
+<?php
+header('Location: /catalogue/');
+exit;
 ?>
-
-<main>
-    <h2>Toutes les annonces</h2>
-    
-    <?php if (empty($articles)): ?>
-        <p>Aucune annonce pour le moment.</p>
-    <?php else: ?>
-        <div class="annonces-grid">
-            <?php foreach ($articles as $article): ?>
-                <div class="annonce-card">
-                    <h3><?= htmlspecialchars($article['titre']) ?></h3>
-                    <p>Prix : <strong><?= $article['prix'] ?> €</strong></p>
-                    <p>Ville : <?= htmlspecialchars($article['ville_nom']) ?></p>
-                    <a href="/items/item.php?id=<?= $article['id'] ?>">Voir le détail</a>
-                </div>
-                <hr>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
-</main>
-
-<?php include '../templates/footer.php'; ?>
